37ca7e8d75420d71f4e5d0675806877074a08f9f
Jethva Amit <amitjethva2707@gmail.com>
2026-07-17 16:13:22 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.81%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int depth =0;
+        for(char x : s){
+            if(x == '('){
+                if(depth>0){
+                    ans +=x;
+                }
+                depth++;
+            } else {
+                depth--;
+                if(depth>0){
+                    ans+=x;
+                }
+            }
+        }
+        return ans;
+    }
+};
