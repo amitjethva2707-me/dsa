e8bba756dfbd46b44733f423db0613ca704968ad
Jethva Amit <amitjethva2707@gmail.com>
2026-07-24 13:21:05 +0530

    Time: 7 ms (95.92%), Space: 8.9 MB (95.42%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -3,15 +3,15 @@ public:
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<double> prev(m+1,0);
-        prev[0]  =1;
+        vector<double> dp(m+1,0);
+        dp[0]  =1;
         for(int i =1;i<=n;i++){
             for(int j =m;j>=1;j--){
                 if(s[i-1] == t[j-1]){
-                    prev[j] = prev[j-1]+ prev[j];
+                    dp[j] = dp[j-1]+ dp[j];
                 } 
             }
         }
-        return (int)prev[m];
+        return (int)dp[m];
     }
 };
