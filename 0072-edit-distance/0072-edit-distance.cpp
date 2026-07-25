e83fb13221b4280b04662390a27313adb00bfc5b
Jethva Amit <amitjethva2707@gmail.com>
2026-07-25 14:41:02 +0530

    Time: 0 ms (100%), Space: 10.4 MB (93.81%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<int> prev(m+1,0);
+        vector<int> curr(m+1,0);
+        for(int j = 0;j<=m;j++){
+            prev[j] = j;
+        }
+        for(int i =1;i<=n;i++){
+            curr[0] =i;
+            for(int j =1;j<=m;j++){
+                if(word1[i-1] == word2[j-1]){
+                    curr[j] = prev[j-1];
+                } else{
+                    curr[j] = 1 + min(prev[j],min(curr[j-1],prev[j-1]));
+                }
+            }
+            prev = curr;
+        }
+        return prev[m];
+    }
+};
