c814d5f1af29665481e845c7a499da5e72e51613
Jethva Amit <amitjethva2707@gmail.com>
2026-07-28 14:57:26 +0530

    Time: 154 ms (59.07%), Space: 121 MB (62.18%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> after(2,vector<int>(3,0));
-        vector<vector<int>> curr (2,vector<int>(3,0));
-        for(int i = prices.size()-1;i>=0;i--){
-            for(int buy =0;buy<=1;buy++){
-               for(int cnt =1;cnt<= 2;cnt++){
-                if(buy ==1){
-                    curr[buy][cnt] = max(-prices[i]+after[0][cnt],after[1][cnt]);
+        int n = prices.size();
+        vector<int> after(5, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            vector<int> cur(5, 0);
+            for (int trasaction = 0; trasaction < 4; trasaction++) {
+                if (trasaction % 2 == 0) {
+                    cur[trasaction] =
+                        max(-prices[i] + after[trasaction + 1],
+                            after[trasaction]);
                 } else {
-                    curr[buy][cnt] = max(prices[i]+after[1][cnt-1],after[0][cnt]);
+                    cur[trasaction] =
+                        max(prices[i] + after[trasaction + 1],
+                            after[trasaction]);
                 }
-               }
             }
-            after = curr;
+            after = cur;
         }
-        return (int)after[1][2];
+        return after[0];
     }
 };
