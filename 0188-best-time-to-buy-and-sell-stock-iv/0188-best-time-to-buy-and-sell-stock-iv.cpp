1b8c10eea41bacc2b2701c77c4c69376d6177993
Jethva Amit <amitjethva2707@gmail.com>
2026-07-28 15:17:52 +0530

    Time: 0 ms (100%), Space: 14.2 MB (99.2%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,9 +3,9 @@ public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
         vector<int> after(2*k+1, 0);
+        vector<int> cur(2*k+1, 0);
         for (int i = n - 1; i >= 0; i--) {
-            vector<int> cur(2*k+1, 0);
-            for (int trasaction = 0; trasaction < 2*k; trasaction++) {
+           for (int trasaction = 2*k-1; trasaction >=0 ;trasaction--) {
                 if (trasaction % 2 == 0) {
                     cur[trasaction] =
                         max(-prices[i] + after[trasaction + 1],
